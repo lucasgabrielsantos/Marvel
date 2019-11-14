8ec5c10d150f3da651b0a514f91fc2ce30aa3675
Lucas Gabriel dos Santos <51340471+lucasgabrielsantos@users.noreply.github.com>
2019-11-14 17:50:22 -0300

    implements loginactivity

--- app/src/main/java/br/com/digitalhouse/projetomarvel/view/activity/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/projetomarvel/view/activity/MainActivity.java
@@ -3,12 +3,21 @@ package br.com.digitalhouse.projetomarvel.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +28,8 @@ import br.com.digitalhouse.projetomarvel.view.Interface.OnClickDetails;
 import br.com.digitalhouse.projetomarvel.view.adapter.ComicsAdapter;
 import br.com.digitalhouse.projetomarvel.viewmodel.ComicsViewModel;
 
+import static br.com.digitalhouse.projetomarvel.view.activity.LoginActivity.GOOGLE_ACCOUNT;
+
 public class MainActivity extends AppCompatActivity implements OnClickDetails {
 
     private RecyclerView recyclerView;
@@ -26,6 +37,10 @@ public class MainActivity extends AppCompatActivity implements OnClickDetails {
     private ComicsAdapter adapter;
     private List<Result> resultList = new ArrayList<>();
     private ComicsViewModel viewModel;
+    private GoogleSignInClient googleSignInClient;
+    private ImageView imgProfile;
+    private TextView nameProfile;
+    private Button btnLogout;
 
     public static final String RESULT_KEY = "result";
 
@@ -35,6 +50,23 @@ public class MainActivity extends AppCompatActivity implements OnClickDetails {
         setContentView(R.layout.activity_main);
 
         initViews();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        pegaOsDados();
+
+        btnLogout.setOnClickListener(v ->
+                googleSignInClient.signOut().addOnCompleteListener(task -> {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }));
+
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
@@ -60,6 +92,9 @@ public class MainActivity extends AppCompatActivity implements OnClickDetails {
         progressBar = findViewById(R.id.progressBar);
         adapter = new ComicsAdapter(resultList, this);
         viewModel = ViewModelProviders.of(this).get(ComicsViewModel.class);
+        imgProfile = findViewById(R.id.imagemlogogoogle);
+        nameProfile = findViewById(R.id.logingoogle);
+        btnLogout = findViewById(R.id.buttonSair);
 
     }
 
@@ -71,4 +106,11 @@ public class MainActivity extends AppCompatActivity implements OnClickDetails {
         intent.putExtras(bundle);
         startActivity(intent);
     }
+
+    private void pegaOsDados() {
+        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+        Picasso.get().load(googleSignInAccount.getPhotoUrl()).into(imgProfile);
+        nameProfile.setText(googleSignInAccount.getDisplayName());
+    }
+
 }
--- app/src/main/java/br/com/digitalhouse/projetomarvel/view/activity/SplashActivity.java
+++ app/src/main/java/br/com/digitalhouse/projetomarvel/view/activity/SplashActivity.java
@@ -44,7 +44,7 @@ public class SplashActivity extends AppCompatActivity {
     }
     private void jump() {
         timer.cancel();
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
